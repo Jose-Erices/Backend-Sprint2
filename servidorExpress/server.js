2d74c2c461509a6df2860db66bfd261188b6c1ec
JoseErices2123 <Jose.erices@avattar.com>
2024-03-20 00:40:23 -0300

    levanto el localhost 8080.

--- servidorExpress/server.js
+++ servidorExpress/server.js
@@ -1,1 +1,32 @@
-console.log("hola Jose")
+import express from "express"
+//import nombre Modulo from "nombreModulo"
+//import Archivo from "ruta/del/archivo.js"
+
+
+//Definimos el servidor
+const server = express ()
+
+//se crea el servidor
+const port = 8080
+const ready = () => console.log("server ready on port"+port)
+
+server.listen (port, ready)
+
+//router
+
+server.get("/", async(requerimientos, respuestas)=>{
+
+    try{
+        return respuestas.status(200).json({
+            response:"coder Api",
+            success: true
+
+        })
+    } catch (error){
+        console.log(error);
+        return respuestas.status(500).json ({
+            response: "coder Api Error",
+            success: false
+        })
+    }
+})
